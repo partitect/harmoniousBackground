4bf3740c027ea37ab59ec3c4655570c821ece6e1
Telat Kaya <telatkaya@yandex.com>
2022-06-23 09:57:29 +0300

    Add mask image on to imgs

--- js/script.js
+++ js/script.js
@@ -10,6 +10,10 @@ $(window).on("load", function () {
   sourceImage.parent(".card").each(function (index) {
     $(this).children("h1").css("color","rgba(" + palette[index][2] + ", 1)")
     $(this).children("p").css("color","rgba(" + palette[index][2] + ", 1)")
+
+    //$(this).children("img").css("mask-mode",`linear-gradient(to bottom, transparent 0, rgba(${palette[index][0]}", 1) 75%)`)
+
+
       $(this).css(
           "box-shadow",
           "0 8px 20px -1px rgba(" + palette[index][0] + ", 1)"
